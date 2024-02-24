82f5c527e6ddd772b1e769c6a43421ab3b7a6b8b
Fulvio Denza <fulviodenza823@gmail.com>
2024-02-24 18:09:49 +0100

    Add context to readme and minor changes to lib.rs

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # notion-echo
 A telegram bot to write your thoughts in Notion from Telegram
+
+# Usage
+Coming soon...
+
+## why making it so complex using napi with rust and c++ splitting the bot in two components?
+Because I wanted to get in trouble.
--- src/lib.rs
+++ src/lib.rs
@@ -3,18 +3,19 @@ use teloxide::prelude::*;
 
 #[napi]
 async fn dice() {
-    println!("Starting throw dice bot...");
+    println!("Starting notion-echo bot...");
 
     let bot = Bot::from_env();
 
     teloxide::repl(bot, |bot: Bot, msg: Message| async move {
-        bot.send_dice(msg.chat.id).await?;
+        bot.send_message(msg.chat.id, "Note saved to Notion")
+            .await?;
         Ok(())
     })
     .await;
 }
 
-// fn access_shared_buffer(mut cx: FunctionContext) -> JsResult<JsUndefined> {
+// fn access_shared_buffer(mut cx: FunctionContext) -> Result<JsUndefined> {
 //     let buffer = cx.argument::<JsArrayBuffer>(0)?;
 //     let slice = cx.borrow(&buffer, |data| {
 //         let slice = data.as_slice::<u8>();
