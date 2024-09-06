7a91f51b790db3b1fe2aafba121ee4254877bc62
fd <fulviodenza823@gmail.com>
2024-09-06 11:25:36 +0200

    Improve error message

--- errors/errors.go
+++ errors/errors.go
@@ -8,7 +8,7 @@ var (
 	ErrNotEnoughArguments = errors.New("not enough arguments")
 	ErrNotRegistered      = errors.New("it looks like you are not registered, try running `/register` command first")
 	ErrSetDefaultPage     = errors.New("error setting default page")
-	ErrPageNotFound       = errors.New("page not found")
+	ErrPageNotFound       = errors.New("page not found, please run /defaultpage command first")
 	ErrRegistering        = errors.New("error registering")
 	ErrDeleting           = errors.New("error deleting user")
 	ErrStateToken         = errors.New("error generating state token, please retry later")
