1f87399c88361209838122f7d691d990dabc10b2
Fulvio <fulvio@civo.com>
2025-02-05 21:26:47 +0100

    Fix error

--- errors/errors.go
+++ errors/errors.go
@@ -14,5 +14,5 @@ var (
 	ErrDeleting           = errors.New("error deleting user")
 	ErrStateToken         = errors.New("error generating state token, please retry later")
 	ErrInternal           = errors.New("it seems we are having internal troubles, please come back later")
-	ErrBotNotAuthorized   = errors.New("it looks like I don't have access to any page, ensure running the command /register and select the pages you want me to access to")
+	ErrBotNotAuthorized   = errors.New("it looks like I don't have access to any page or the page contains some children, ensure running the command /register and select the pages you want me to access to")
 )
